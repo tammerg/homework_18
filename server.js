ae13f984cdf6e60d6e73e389c8817306d63a770d
Tammer <tammergalal@gmail.com>
2016-03-13 17:33:07 -0400

    working on scraping on button click and showing meta data in the console. Should have commited earlier, will remember to do so more often in the future.

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
 var parser = require('body-parser');
-var logger = require('cheerio');
+var cheerio = require('cheerio');
+var mongoose = require('mongoose');
 var request = require('request');
-var PORT = 8080;
+mongoose.connect('mongodb://localhost/homework18DB');
+var PORT = process.env.PORT || 8080;
+
+app.use('/scripts', express.static('public/scripts'));
+app.use('/css', express.static('public/stylesheets'));
+app.use('/img', express.static('public/img'));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.listen(PORT, function(req, res){
   console.log('You are listening on port %s', PORT);
 });
-
 
 //Express Routes
 app.get('/', function(req, res){
